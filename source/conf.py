bd7804124cac4331af73420bdb4ef1fe8b0a8e72
TAMADA <a388087@gmail.com>
2022-11-28 20:15:31 +0800

    5st

--- source/conf.py
+++ source/conf.py
@@ -13,12 +13,10 @@ author = 'zxf'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['recommonmark','nbshpinx']
+extensions = ['recommonmark', 'nbshpinx']
 
 templates_path = ['_templates']
 exclude_patterns = []
-
-
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
