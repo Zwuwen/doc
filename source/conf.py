9ec1c06d568bbcdeba1ea372f729ca28399058ad
TAMADA <a388087@gmail.com>
2022-12-21 13:10:31 +0800

    6st

--- source/conf.py
+++ source/conf.py
@@ -21,10 +21,10 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-import sphinx_rtd_theme
+import sphinx_pdj_theme
 
-html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme = "sphinx_pdj_theme"
+html_theme_path = [sphinx_pdj_theme.get_html_theme_path()]
 
 latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
