fb332c3df18a27d0097583a4f53f45485bcee2f8
TAMADA <a388087@gmail.com>
2022-12-21 13:20:22 +0800

    6st

--- source/conf.py
+++ source/conf.py
@@ -13,7 +13,7 @@ author = 'zxf'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['recommonmark', 'nbsphinx', 'sphinx_rtd_theme']
+extensions = ['recommonmark', 'nbsphinx', 'sphinx_pdj_theme']
 
 templates_path = ['_templates']
 exclude_patterns = []
